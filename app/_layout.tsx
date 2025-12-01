a9faa8644b3e3240c1dcb9bd5946e495206abbb1
Gayan65 <gayan65728@yahoo.com>
2025-12-01 05:33:36 +0200

    auth add

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,25 +1,35 @@
-import { Stack, useRouter } from "expo-router";
+import { AuthProvider, useAuth } from "@/lib/auth-context";
+import { Stack, useRouter, useSegments } from "expo-router";
 import { useEffect } from "react";
 
 function RouteGuard({ children }: { children: React.ReactNode }) {
     const router = useRouter();
-    const isAuth = false;
+    const { user, isLoadingUser } = useAuth();
+    const segment = useSegments();
 
     useEffect(() => {
-        if (!isAuth) {
+        const inAuthGroup = segment[0] === "auth";
+        if (!user && !inAuthGroup && !isLoadingUser) {
             router.replace("/auth");
+        } else if (user && inAuthGroup && !isLoadingUser) {
+            router.replace("/");
         }
-    });
+    }, [user, segment]);
 
     return <>{children}</>;
 }
 
 export default function RootLayout() {
     return (
-        <RouteGuard>
-            <Stack>
-                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-            </Stack>
-        </RouteGuard>
+        <AuthProvider>
+            <RouteGuard>
+                <Stack>
+                    <Stack.Screen
+                        name="(tabs)"
+                        options={{ headerShown: false }}
+                    />
+                </Stack>
+            </RouteGuard>
+        </AuthProvider>
     );
 }
--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,8 +1,7 @@
 import { Account, Client } from "react-native-appwrite";
 
 export const client = new Client()
-    .setEndpoint(process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT!)
     .setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!)
-    .setPlatform(process.env.EXPO_PUBLIC_APPWRITE_PLATFORM!);
+    .setEndpoint(process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT!);
 
 export const account = new Account(client);
