791768575b1749e131775968e48bcd052f564cad
Gayan65 <gayan65728@yahoo.com>
2025-12-01 17:50:13 +0200

    login bug fixed

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,21 @@
+import { useAuth } from "@/lib/auth-context";
 import { Text, View } from "react-native";
+import { Button } from "react-native-paper";
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+    const { signOut } = useAuth();
+    return (
+        <View
+            style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+            }}
+        >
+            <Text>Edit app/index.tsx to edit this screen.</Text>
+            <Button mode="text" onPress={signOut} icon={"logout"}>
+                Sign Out
+            </Button>
+        </View>
+    );
 }
